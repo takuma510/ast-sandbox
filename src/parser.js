512a0215fa1798b0a4d73f85bfb07cca923464c5
Takuma Goto <takuma510@gmail.com>
2019-10-26 18:07:05 +0900

    ast pretty-print

--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,10 @@
 // https://babeljs.io/docs/en/babel-parser
 const babelParser = require('@babel/parser');
+const printAST = require('ast-pretty-print');
 
 const code = '1 + 1';
 
 const ast = babelParser.parse(code);
 
-console.log(ast);
+//console.log(ast);
+console.log(printAST(ast, true));
