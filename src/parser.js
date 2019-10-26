ba27c738bba4508ebddf5e3ff5d054b6f303b877
Takuma Goto <takuma510@gmail.com>
2019-10-26 19:27:49 +0900

    JSON.stringify(ast, replacer, ' ') logger

--- src/parser.js
+++ src/parser.js
@@ -3,8 +3,27 @@ const babelParser = require('@babel/parser');
 const printAST = require('ast-pretty-print');
 
 const code = '1 + 2 * (3 + 4)';
-
 const ast = babelParser.parse(code);
 
+const isNode = (obj) => {
+  if (typeof obj !== 'object') {
+    return false;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.find(v => isNode(v)) !== undefined;
+  }
+
+  return obj && 'constructor' in obj && obj.constructor.name === 'Node';
+};
+const replacer = (key, value) => {
+  if (!key || key === 'type' || isNode(value)) {
+    return value;
+  }
+
+  return undefined;
+};
+
 //console.log(ast);
-console.log(printAST(ast, true));
+//console.log(printAST(ast, true));
+console.log(JSON.stringify(ast, replacer, ' '));
