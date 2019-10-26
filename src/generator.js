b59696f62f5c350f9b6e3ef42f5862cce7496628
Takuma Goto <takuma510@gmail.com>
2019-10-27 01:36:49 +0900

    @babel/generator

---
+++ src/generator.js
@@ -0,0 +1,16 @@
+// https://babeljs.io/docs/en/babel-generator
+
+const generate = require('@babel/generator').default;
+
+const ast = {
+  type: 'ExpressionStatement',
+  expression: {
+    type: 'BinaryExpression',
+    operator: '+',
+    left: {type: 'NumericLiteral', value: 1},
+    right: {type: 'NumericLiteral', value: 2}
+  }
+};
+
+const {code, map} = generate(ast);
+console.log(code);
